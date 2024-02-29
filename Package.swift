c198f5fc4405820425ba6493f8a0fadecd995e9b
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-02-28 22:54:04 -0500

    Add Alamofire dependency

--- Package.swift
+++ Package.swift
@@ -11,11 +11,16 @@ let package = Package(
             name: "PrefabCloud",
             targets: ["PrefabCloud"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.8.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PrefabCloud"),
+            name: "PrefabCloud",
+            dependencies: ["Alamofire"]
+        ),
         .testTarget(
             name: "PrefabCloudTests",
             dependencies: ["PrefabCloud"]),
